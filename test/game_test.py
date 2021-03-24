59f3e37e7fc67cf80c58ef038f7cb18224ab9261
Shana Shin <sshshana@gmail.com>
2021-03-25 06:57:30 +0900

    testing

--- test/game_test.py
+++ test/game_test.py
@@ -1,4 +1,16 @@
 
-# TODO: import some code
 
-# TODO: test the code
+from app.game import determine_winner
+
+def test_determination_of_the_winner():
+    assert determine_winner("rock", "rock") == "It's a tie!"
+    assert determine_winner("rock", "paper") == "The computer wins"
+    assert determine_winner("rock", "scissors") == "The user wins"
+
+    assert determine_winner("paper", "rock") == "The user wins"
+    assert determine_winner("paper", "paper") == "It's a tie!"
+    assert determine_winner("paper", "scissors") == "The computer wins"
+
+    assert determine_winner("scissors", "rock") == "The computer wins"
+    assert determine_winner("scissors", "paper") == "The user wins"
+    assert determine_winner("scissors", "scissors") == "It's a tie!"
