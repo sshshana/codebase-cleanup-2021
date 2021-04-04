0de41a9a6865e79bb983ae64f74bc7c2694f586c
Shana Shin <sshshana@gmail.com>
2021-04-04 20:27:36 +0900

    Update the docstring

--- test/shopping_test.py
+++ test/shopping_test.py
@@ -1,4 +1,31 @@
+import os
+from pandas import read_csv
+from app.shopping import format_usd, lookup_product
 
-# TODO: import some code
+def to_usd(my_price):
+    """
+    Formats a number as USD with dollar sign and two decimals (and also thousands separator)
 
-# TODO: test the code
+    Params my_price is a number (int or float) that we want to format
+
+    Examples: format_usd(10) / Result: $10.00
+    """
+    return f"${my_price:,.2f}"
+
+
+def test_lookups():
+    valid_result = lookup_product("8", mock_products)
+    assert valid_result == {
+        'aisle': "Aisle C",
+        'department': 'snacks',
+        'id': '8',
+        'name': 'Product 8'
+        'price': 10.0
+
+    }
+
+
+
+mock_products_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
+mock_products_df = read_csv(products_filepath)
+mock_products = products_df.to_dict("records")
